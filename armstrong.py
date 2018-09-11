80a58c23f5ad5ddd01f042784b2097ab4bf163db
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-09-11 11:46:03 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,14 @@
+lower,upper=map(int,raw_input().split())
+for num in range(lower+1,upper):
+    order=len(str(num))
+    sum=0
+    temp=num
+    while(temp>0):
+        digit=temp%10
+        sum+=digit**order
+        temp//=10
+    if(num==sum):
+        print sum
+        
+        
+        
